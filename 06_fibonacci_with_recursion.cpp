d8d42e3bfe8dd92aae3ab0b5d4cfeb0398146ed0
Gokulraj-123 <123939891+Gokulraj-123@users.noreply.github.com>
2023-08-13 18:36:06 +0530

    Update 06_fibonacci_with_recursion.cpp

--- 06_fibonacci_with_recursion.cpp
+++ 06_fibonacci_with_recursion.cpp
@@ -1,20 +1,19 @@
-#include <stdio.h>
-int fibonacci(int n) {
-    if (n <= 1) {
-        return n;
-    }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+#include<stdio.h>
+int fibonacci(int n){
+	if(n<=1){
+		return n;
+	}
+	else{
+		return fibonacci(n-1)+fibonacci(n-2);
+	}
 }
-int main() {
-    int n;
-    printf("Enter the position to calculate Fibonacci number: ");
-    scanf("%d", &n);
-    if (n < 0) {
-        printf("Invalid position. Please enter a non-negative number.\n");
-    } else {
-        int result = fibonacci(n);
-        printf("Fibonacci number at position %d is: %d\n", n, result);
-    }
-    return 0;
+int main(){
+	int n,i;
+	printf("Enter the value: ");
+	scanf("%d", &n);
+	printf("Fibonacci sequences: ");
+	for(i=0;i<n;i++){
+		printf("%d ", fibonacci(i));
+	}
+	return 0;
 }
-
