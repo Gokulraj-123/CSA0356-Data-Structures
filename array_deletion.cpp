5c07124f9eaaf9d3959cf1e603481261c103d4a3
Gokulraj-123 <123939891+Gokulraj-123@users.noreply.github.com>
2023-08-13 19:28:44 +0530

    Update array_deletion.cpp

--- array_deletion.cpp
+++ array_deletion.cpp
@@ -1,23 +1,28 @@
 #include <stdio.h>
-int main()
-{
-	int array[100], p, c, n;
-	printf("Enter the number of elements of the array : ");
-	scanf("%d", &n);
-	printf("\nInput the array elements : ");
-	for (c = 0; c < n; c++)
-	scanf("%d", &array[c]);
-	printf("\nEnter the position : ");
-	scanf("%d", &p);
-	if (position >= n+1)
-	printf("\nDeletion not possible.\n");
-	else
-	{
-		for (c = p – 1; c < n – 1; c++)
-		array[c] = array[c+1];
-		printf("\nArray after deletion : ");
-		for (c = 0; c < n – 1; c++)
-		printf("%d\n", array[c]);
-	}
-return 0;
+
+int main() {
+    int array[100], p, i, n;
+
+    printf("Enter the number of elements of the array: ");
+    scanf("%d", &n);
+
+    printf("\nInput the array elements: ");
+    for (i = 0; i < n; i++)
+        scanf("%d", &array[i]);
+
+    printf("\nEnter the position: ");
+    scanf("%d", &p);
+
+    if (p >= n + 1)
+        printf("\nDeletion not possible.\n");
+    else {
+        for (i = p - 1; i < n - 1; i++)
+            array[i] = array[i + 1];
+
+        printf("\nArray after deletion: ");
+        for (i = 0; i < n - 1; i++)
+            printf("%d\n", array[i]);
+    }
+
+    return 0;
 }
